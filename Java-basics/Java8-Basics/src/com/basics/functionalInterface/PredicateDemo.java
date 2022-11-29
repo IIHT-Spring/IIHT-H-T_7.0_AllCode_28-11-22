374594c13195f0cefa6c5300e3bd78aa242a72da
ngactive99 <ngactive99@gmail.com>
2022-11-29 15:00:50 +0530

    added predicate functional interface

--- Java-basics/Java8-Basics/src/com/basics/functionalInterface/PredicateDemo.java
+++ Java-basics/Java8-Basics/src/com/basics/functionalInterface/PredicateDemo.java
@@ -1,5 +1,20 @@
 package com.basics.functionalInterface;
 
-public class PredicateDemo {
+import java.util.function.Predicate;
+
+public class PredicateDemo implements Predicate<Integer> {
+
+	@Override
+	public boolean test(Integer t) {
+
+		if (t % 2 == 0) {
+			return true;
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		Predicate<Integer> predicate= new PredicateDemo();
+		System.out.println(predicate.test(5));
+	}
 
 }
