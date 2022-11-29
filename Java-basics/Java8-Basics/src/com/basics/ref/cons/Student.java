a5bf8d006f967312bdd2c234ff626c1d0c410085
ngactive99 <ngactive99@gmail.com>
2022-11-29 17:25:32 +0530

    added contructor reference

---
+++ Java-basics/Java8-Basics/src/com/basics/ref/cons/Student.java
@@ -0,0 +1,13 @@
+package com.basics.ref.cons;
+
+public class Student {
+	
+	public Student() {
+		System.out.println("Creating object of the student class");
+	}
+	
+	public void display() {
+		System.out.println("this is a student");
+	}
+
+}
