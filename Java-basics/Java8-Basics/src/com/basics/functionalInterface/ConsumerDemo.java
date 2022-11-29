8876b04623e45d6547eeba30e31fe7c6ce9ff8f3
ngactive99 <ngactive99@gmail.com>
2022-11-29 14:32:06 +0530

    added consumer functional interface demo

---
+++ Java-basics/Java8-Basics/src/com/basics/functionalInterface/ConsumerDemo.java
@@ -0,0 +1,14 @@
+package com.basics.functionalInterface;
+
+import java.util.function.Consumer;
+
+public class ConsumerDemo  {
+
+	public static void main(String[] args) {
+	  Consumer<Integer> consumer=(t) ->
+			System.out.println("printing : " + t);	
+			consumer.accept(4000);
+	}
+	
+
+}
