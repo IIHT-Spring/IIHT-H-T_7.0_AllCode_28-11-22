18a22e7762af3a07ad3c12d8e518e91fe83767fe
ngactive99 <ngactive99@gmail.com>
2022-11-29 16:25:17 +0530

    basic method ref done with two classes and an interface

---
+++ Java-basics/Java8-Basics/src/com/basics/ref/Project.java
@@ -0,0 +1,15 @@
+package com.basics.ref;
+
+import java.time.LocalDate;
+
+public class Project {
+	
+	public static void projectProgress() {
+		
+System.out.println("Project is making progress");
+System.out.println("project status was approved by client");
+LocalDate now = LocalDate.now();
+System.out.println("on " + now.toString() );
+	}
+
+}
