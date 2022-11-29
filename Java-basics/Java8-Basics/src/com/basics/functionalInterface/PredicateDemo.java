3f45814fb77e07c3d80f483a88954215070ce9f6
ngactive99 <ngactive99@gmail.com>
2022-11-29 15:21:05 +0530

    added predicate with lambda

--- Java-basics/Java8-Basics/src/com/basics/functionalInterface/PredicateDemo.java
+++ Java-basics/Java8-Basics/src/com/basics/functionalInterface/PredicateDemo.java
@@ -1,20 +1,15 @@
 package com.basics.functionalInterface;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
-public class PredicateDemo implements Predicate<Integer> {
+public class PredicateDemo {
 
-	@Override
-	public boolean test(Integer t) {
-
-		if (t % 2 == 0) {
-			return true;
-		}
-		return false;
-	}
 	public static void main(String[] args) {
-		Predicate<Integer> predicate= new PredicateDemo();
-		System.out.println(predicate.test(5));
+		
+		List<Integer> numberList = Arrays.asList(1,2,3,4,5);
+		
+		numberList.stream().filter((t) -> t%2 ==0).forEach( number-> System.out.println("printing: " + number));
 	}
-
 }
