0132b99f131e4e4b810722df385b7dd5533819d1
ngactive99 <ngactive99@gmail.com>
2022-11-29 17:00:39 +0530

    added method ref

--- Java-basics/Java8-Basics/src/com/basics/ref/RefDemo.java
+++ Java-basics/Java8-Basics/src/com/basics/ref/RefDemo.java
@@ -6,8 +6,16 @@ public class RefDemo {
 		System.out.println("Method reference demo");
 		
 		// provide the implementation of IWork
+		//classname::methodname
 	IWork iWork = Project::projectProgress;
 		iWork.doProject();
+		
+		
+	Runnable runnable = Project::threadTask;
+	Thread t1 = new Thread(runnable);
+	t1.start();
 	}
+	
+	// create a method ref to that method in the project class
 
 }
