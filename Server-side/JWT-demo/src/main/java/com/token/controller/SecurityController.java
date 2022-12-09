e5c7b2ded6e6b7f2123cc7bb1f8ec84741f4f3ca
ng.test88 <ng.test88@gmail.com>
2022-12-09 15:14:05 +0530

    added spring security

---
+++ Server-side/JWT-demo/src/main/java/com/token/controller/SecurityController.java
@@ -0,0 +1,15 @@
+package com.token.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+	
+	
+	@GetMapping("/")
+	public String home() {
+		return "hello and wecome to home page ";
+	}
+
+}
