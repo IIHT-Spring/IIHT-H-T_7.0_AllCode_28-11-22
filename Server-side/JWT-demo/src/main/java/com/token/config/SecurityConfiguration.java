b228a13bd53ae089f42f684a6ca24be456652182
ng.test88 <ng.test88@gmail.com>
2022-12-09 17:58:36 +0530

    created jwt token

--- Server-side/JWT-demo/src/main/java/com/token/config/SecurityConfiguration.java
+++ Server-side/JWT-demo/src/main/java/com/token/config/SecurityConfiguration.java
@@ -1,8 +1,11 @@
 package com.token.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,13 +15,30 @@ import com.token.service.UserService;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private UserService userService;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService);
+	}
+
+	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf()
+		.disable()
+		.authorizeRequests()
+		.antMatchers("/auth")
+		.permitAll()
+		.anyRequest()
+		.authenticated();
 	}
 
 }
